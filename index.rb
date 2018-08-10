0704452432aa825d9dec10e061b3e3458b4f5ff4
chloexiang217 <chloe.xiang217@gmail.com>
2018-08-10 13:54:12 +0000

    automatically backed up by learn

--- index.rb
+++ index.rb
@@ -7,33 +7,9 @@ ingredients = [
 ]
 
 def sandwich_request(ingredients, order) 
-  # your code here!
+  puts ingredients[0][1]
+  puts ingredients[1]
+  puts ingredients[1][0][0]
 end
 
-# example city_info hash
-cities = {
-  :new_york => { 
-    :mayor => "Bill DeBlasio",
-    :population => 8337000,
-    :website => "http://www.nyc.gov"
-  },
-  :los_angeles => { 
-    :mayor => "Eric Garcetti",
-    :population => 3884307,
-    :website => "http://www.lacity.org"
-  },
-  :miami => { 
-    :mayor => "Tomás Regalado",
-    :population => 419777,
-    :website => "http://www.miamigov.com"
-  },
-  :chicago => { 
-    :mayor => "Rahm Emanuel",
-    :population => 2695598,
-    :website => "http://www.cityofchicago.org/"
-  }
-}
 
-def city_populations(cities)
-  # your code here!
-end
